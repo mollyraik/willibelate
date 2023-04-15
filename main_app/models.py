96cf295d7d342f4661777efd73b192b24f88b6d8
mollyraik <molly.raik@gmail.com>
2023-04-15 14:13:49 -0400

    wip: initial subway and station models will need tweaking

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Route(models.Model):
+    route_id = models.CharField(max_length=2, primary_key=True)
+    agency_id = models.CharField(max_length=10, default='MTA NYCT')
+    route_short_name = models.CharField(max_length=2)
+    route_long_name = models.CharField(max_length=50)
+    route_desc = models.TextField()
+    route_type = models.IntegerField(default=1)
+    route_url = models.CharField(max_length=50)
+    route_color = models.CharField(max_length=6, null=True)
+    route_text_color = models.CharField(max_length=6, null=True)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.route_id
+
+class Station(models.Model):
+    stop_id = models.CharField(max_length=4)
+    name = models.CharField(max_length=50)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    parent_id = models.CharField(max_length=4)
+    json_id = models.CharField(max_length=4, null=True)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+    
