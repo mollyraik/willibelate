64a443dd014b6a5d6b358a9973de3eb62d0f7c55
mollyraik <molly.raik@gmail.com>
2023-04-18 22:33:36 -0400

    wip: adds paths for adding favs

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('test/', views.test, name='test'),
+    path('test/', views.test, name='test'),
     path('accounts/signup/', views.signup, name='signup'),
     path('line/favorites/', views.subway_list, name='favorite_subways'),
     path('station/favorites/', views.station_list, name='favorite_stations'),
