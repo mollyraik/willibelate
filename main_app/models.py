b98adb90e8e7eafef7382702ce25911c0d582c17
mollyraik <molly.raik@gmail.com>
2023-04-19 22:48:09 -0400

    fixed models for favorites

--- main_app/models.py
+++ main_app/models.py
@@ -5,29 +5,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Route(models.Model):
     route_id = models.CharField(max_length=2, primary_key=True)
-    agency_id = models.CharField(max_length=10, default='MTA NYCT')
-    route_short_name = models.CharField(max_length=2)
-    route_long_name = models.CharField(max_length=50)
-    route_desc = models.TextField()
-    route_type = models.IntegerField(default=1)
-    route_url = models.CharField(max_length=50)
-    route_color = models.CharField(max_length=6, null=True)
-    route_text_color = models.CharField(max_length=6, null=True)
-    is_favorite = models.BooleanField(default=False)
-    user = models.ManyToManyField(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.route_id
+    
 
 class Station(models.Model):
-    stop_id = models.CharField(max_length=4)
-    name = models.CharField(max_length=50)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    parent_id = models.CharField(max_length=4)
-    json_id = models.CharField(max_length=4, null=True)
-    is_favorite = models.BooleanField(default=False)
-    user = models.ManyToManyField(User)
+    station_id = models.CharField(max_length=10, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
