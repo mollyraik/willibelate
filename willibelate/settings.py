13627e45540d2307e44bdf44049ffaac1036c768
mollyraik <molly.raik@gmail.com>
2023-04-25 17:16:56 -0400

    debug changed to false

--- willibelate/settings.py
+++ willibelate/settings.py
@@ -28,9 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'willibelate.herokuapp.com',
+]
 
 
 # Application definition
