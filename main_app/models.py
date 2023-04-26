c3bb37f108db5db08734d2b286c308e11e679078
mollyraik <molly.raik@gmail.com>
2023-04-26 16:58:50 -0400

    updates fave models

--- main_app/models.py
+++ main_app/models.py
@@ -4,16 +4,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Route(models.Model):
-    route_id = models.CharField(max_length=2, primary_key=True)
+    id = models.BigAutoField(primary_key=True)
+    route_id = models.CharField(max_length=2)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timeAdded = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.route_id
     
 
 class Station(models.Model):
-    station_id = models.CharField(max_length=10, primary_key=True)
+    id = models.BigAutoField(primary_key=True)
+    station_id = models.CharField(max_length=10)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timeAdded = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
